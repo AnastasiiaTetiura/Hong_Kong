e243cda5332f6ab37ab56f9fc1a6e7b68a36127d
AnastasiiaTetiura <n.tetiura@gmail.com>
2025-02-25 23:09:08 -0800

    adding first files for description

---
+++ main/Main_page.py
@@ -0,0 +1,12 @@
+print("welcom to HK")
+island = 0
+print(input("Choose the island of your destination - "))
+if island == "Hong King":
+    print("You will go to Victoria Peak")
+elif island == "Lanau":
+    print("You will visit Tian Buddha")
+else:
+    print("You chose non touristic spot")
+
+
+
